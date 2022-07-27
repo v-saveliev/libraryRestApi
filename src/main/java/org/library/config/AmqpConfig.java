586582fd8404bbb7839375ba52c1121df6564ae1
test-chaos <51213139+v-saveliev@users.noreply.github.com>
2022-07-27 14:00:03 +0300

    change rabbitamqp settings

--- src/main/java/org/library/config/AmqpConfig.java
+++ src/main/java/org/library/config/AmqpConfig.java
@@ -1,10 +1,14 @@
 package org.library.config;
 
+import org.library.amqp.AmqpListener;
+import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,8 +26,15 @@ public class AmqpConfig {
     }
 
     @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
     public RabbitTemplate rabbitTemplate() {
-        return new RabbitTemplate(connectionFactory());
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
     }
 
     @Bean
@@ -31,4 +42,15 @@ public class AmqpConfig {
         return new Queue("authors");
     }
 
+    @Bean
+    public SimpleMessageListenerContainer listenerContainer(AmqpListener amqpListener) {
+        SimpleMessageListenerContainer listenerContainer = new SimpleMessageListenerContainer();
+        listenerContainer.setConnectionFactory(connectionFactory());
+        listenerContainer.setQueueNames("authors");
+        listenerContainer.setMessageListener(amqpListener);
+        listenerContainer.setAcknowledgeMode(AcknowledgeMode.AUTO);
+        listenerContainer.setConcurrency("4");
+        listenerContainer.setPrefetchCount(20);
+        return listenerContainer;
+    }
 }
